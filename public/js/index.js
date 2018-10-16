6bee4307a1cd251492de7f933afa87ebb9334834
Raphael Kottakal <raphael@webmanifestation.com>
2018-10-17 00:16:53 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-  
-  socket.emit('createMessage', {
-    from: 'client',
-    text: 'Heeeeleooooo!'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New connection');
 
-  socket.emit('newMessage', {
-    from: 'server',
-    text: 'hello world',
-    createdAt: 1234
-  });
-
   socket.on('createMessage',(message) => {
     console.log('Message to create', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect',(socket) => {
